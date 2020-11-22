695aa1cf2557ddcc7e462fda2f2f283b9765badb
Alec DiFederico <alecdifederico@gmail.com>
2020-11-22 14:30:21 -0500

    initial commit

---
+++ boston_locations.py
@@ -0,0 +1,55 @@
+# Radius is in meters, 1600 meters is roughly 1 mile
+radius = 2000
+
+boston_locations = [
+  {
+    'latitude': 42.359239,
+    'longitude': -71.059150,
+    'radius': radius
+  },
+  {
+    'latitude': 42.372553,
+    'longitude': -71.026707,
+    'radius': radius
+  },
+  {
+    'latitude': 42.332474,
+    'longitude': -71.039749,
+    'radius': radius
+  },
+  {
+    'latitude': 42.340467,
+    'longitude': -71.088845,
+    'radius': radius
+  },
+  {
+    'latitude': 42.314582,
+    'longitude': -71.070988,
+    'radius': radius
+  },
+  {
+    'latitude': 42.350614,
+    'longitude': -71.143434,
+    'radius': radius
+  },
+  {
+    'latitude': 42.286402,
+    'longitude': -71.068414,
+    'radius': radius
+  },
+  {
+    'latitude': 42.282592,
+    'longitude': -71.156475,
+    'radius': radius
+  },
+  {
+    'latitude': 42.269640,
+    'longitude': -71.116652,
+    'radius': radius
+  },
+  {
+    'latitude': 42.299986,
+    'longitude': -71.111333,
+    'radius': radius
+  },
+]
